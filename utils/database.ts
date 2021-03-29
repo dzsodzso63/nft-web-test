df4680acb967894324c52eb10a11f34835ca57dc
Dzso Pengo <jozsef.pengo@formlabs.com>
2021-03-30 00:29:48 +0200

    show

--- utils/database.ts
+++ utils/database.ts
@@ -41,12 +41,13 @@ export const putItem = async (item: {[key: string]: DDBAttributeSource;}) => {
 };
 
 const runCommand = async (command: any) => {
-  console.log('Command', command)
-
+  
   const credentials = process.env.AWS_ACCESS_KEY_ID_TILOS && process.env.AWS_SECRET_ACCESS_KEY_TILOS && {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID_TILOS,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TILOS,
   };
+  
+  console.log('Command', credentials, command)
 
   const dbclient = new DynamoDBClient({ 
     region: 'us-east-2',
