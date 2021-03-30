ae6fb5835ccd4f003b8c08edaa6cf1942dcd67c3
Mark Vujevits <vujevits.mark@gmail.com>
2021-03-30 22:24:21 +0200

    remove commented code

--- pages/api/token/[token].ts
+++ pages/api/token/[token].ts
@@ -14,31 +14,3 @@ export default (request: VercelRequest, response: VercelResponse) => {
     response.status(200).send(data);
   }
 };
-// import express from "express";
-// import { HOST } from "./constants";
-// import db from "./database";
-
-// const PORT = process.env.PORT || 5000;
-
-// const app = express().set("port", PORT);
-
-// // Static public files
-// app.use(express.static("public"));
-
-// app.get("/", (_req, res) => {
-//   res.send("Get ready for OpenSea!");
-// });
-
-// app.get("/api/token/:token_id", (req, res) => {
-//   const tokenId = req.params.token_id;
-//   const bayjor = db[tokenId];
-//   const data = {
-//     ...bayjor,
-//     image: `${HOST}/images/${tokenId}.png`,
-//   };
-//   res.send(data);
-// });
-
-// app.listen(app.get("port"), () => {
-//   console.log("Node app is running on port", app.get("port"));
-// });
